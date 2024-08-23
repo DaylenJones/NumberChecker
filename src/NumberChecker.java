9c88d62d74d3435ce121f8557fd3b97bad9c1724
Daylen Jones <jonesday001@warren.k12.in.us>
2024-08-23 13:27:17 -0400

    update

---
+++ src/NumberChecker.java
@@ -0,0 +1,15 @@
+public class NumberChecker {
+    public static void main(String[] args) {
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0) {
+            System.out.println("negative");
+        } else {
+            System.out.println("zero");
+
+        }
+    }
+}
+
+public static void main(String[] args)
+}
